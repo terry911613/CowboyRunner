4e5eb1152601d4d66c7d6d21645c04ffc74e5a82
TaiYiLee <terry911613@gmail.com>
2022-08-23 23:42:31 +0800

    UPDATED: Add background and ground

--- CowboyRunner/Scene/GameplayScene.swift
+++ CowboyRunner/Scene/GameplayScene.swift
@@ -10,6 +10,31 @@ import SpriteKit
 class GameplayScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        setup()
+    }
+    
+    func setup() {
+        setupBG()
+        setupGrounds()
+    }
+    
+    func setupBG() {
+        for i in 0...2 {
+            let bg = SKSpriteNode(imageNamed: "BG")
+            bg.name = "BG"
+            bg.position = CGPoint(x: CGFloat(i) * bg.size.width, y: 0)
+            bg.zPosition = 0
+            addChild(bg)
+        }
+    }
+    
+    func setupGrounds() {
+        for i in 0...2 {
+            let bg = SKSpriteNode(imageNamed: "Ground")
+            bg.name = "Ground"
+            bg.position = CGPoint(x: CGFloat(i) * bg.size.width, y: -(frame.size.height / 2))
+            bg.zPosition = 3
+            addChild(bg)
+        }
     }
 }
