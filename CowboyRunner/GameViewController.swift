5fc22c280a41fd75d40469e755bc4a587840292d
TaiYiLee <terry911613@gmail.com>
2022-08-23 23:25:33 +0800

    UPDATED: Init

--- CowboyRunner/GameViewController.swift
+++ CowboyRunner/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = GameplayScene(fileNamed: "GameplayScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -28,6 +28,7 @@ class GameViewController: UIViewController {
             
             view.showsFPS = true
             view.showsNodeCount = true
+            view.showsPhysics = true
         }
     }
 
---
+++ CowboyRunner/Scene/GameplayScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameplayScene.swift
+//  CowboyRunner
+//
+//  Created by 李泰儀 on 2022/8/23.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
